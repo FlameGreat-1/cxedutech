bf6fbdd0f26681ad6fa643316d8c852c6284a20b
Innocent Iziogo <innocentizi@outlook.com>
2026-03-31 00:46:08 +0000

    feat(PublicLayout): integrate FilterBar below Header
    - Added FilterBar between Header and main content
    - FilterBar renders on all public pages (sits below header, before hero/page content)
    - Maintains existing layout structure

--- inflexa/client/src/components/layout/PublicLayout.tsx
+++ inflexa/client/src/components/layout/PublicLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import FilterBar from './FilterBar';
 import Footer from './Footer';
 import ToastContainer from '@/components/common/Toast';
 
@@ -7,6 +8,7 @@ export default function PublicLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <FilterBar />
       <main className="flex-1">
         <Outlet />
       </main>
